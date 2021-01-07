5e65451f64d290b30d8c4c4130a42ca3421286c1
Alexander Petrovski <apfb11@gmail.com>
2021-01-07 16:11:35 -0500

    review a4q1

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //Roberto Problem 2
 document.getElementById("problem2").addEventListener("mouseover", () => window.alert("Hey, I told you not to hover over me!"))
+
+//Alexander Problem 1
+document.getElementById("button1").addEventListener("click", () => window.alert("I'm right!")
+document.getElementById("button2").addEventListener("click", () => window.alert("No, I'm right!")
