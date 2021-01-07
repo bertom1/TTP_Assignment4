000f43816d13c4a7e65c4f42e31c38dcac5a8bc7
Alexander Petrovski <apfb11@gmail.com>
2021-01-07 17:12:00 -0500

    q1

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 document.getElementById("problem2").addEventListener("mouseover", () => window.alert("Hey, I told you not to hover over me!"))
 
 //Alexander Problem 1
-document.getElementById("button1").addEventListener("click", () => window.alert("I'm right!")
-document.getElementById("button2").addEventListener("click", () => window.alert("No, I'm right!")
+document.getElementById("button1").addEventListener("click", () => document.getElementById("text").innerHTML = "I'm right")
+document.getElementById("button2").addEventListener("click", () => document.getElementById("text").innerHTML = "No, I'm right!")
